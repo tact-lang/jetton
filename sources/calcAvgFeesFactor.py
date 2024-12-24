c024143534ea58203b3b7c7a00db2151663f5b12
b4cksl4sh <shvandre@mail.ru>
2024-12-24 12:25:00 +0300

    Removed masterchain, added script for calculating fees

---
+++ sources/calcAvgFeesFactor.py
@@ -0,0 +1,11 @@
+refferenceValues = [0.0099692, 0.0053376, 0.002827999]
+newValues = [0.014694, 0.007738799, 0.004915999]
+
+def calcAvgFeesFactor(refferenceValues, newValues):
+    avgFeesFactor = []
+    for i in range(len(refferenceValues)):
+        avgFeesFactor.append(newValues[i] / refferenceValues[i])
+    return sum(avgFeesFactor) / len(avgFeesFactor)
+
+print(*(str(x) + " | " for x in newValues), end="")
+print(round(calcAvgFeesFactor(refferenceValues, newValues), 2), end="x |")
