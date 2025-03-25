2ba938a3260cfba634458231d3bf35d3675a9c04
b4cksl4sh <andrei.sh@ton.org>
2025-03-25 15:48:46 +0300

    Updated benches, adjusted toncenter schema

--- src/utils/toncenter.ts
+++ src/utils/toncenter.ts
@@ -5,10 +5,10 @@ import {expect} from "@jest/globals"
 
 const tokenSchema = z.object({
     type: z.string(),
-    name: z.string(),
-    symbol: z.string(),
-    description: z.string(),
-    image: z.string(),
+    name: z.string().optional(),
+    symbol: z.string().optional(),
+    description: z.string().optional(),
+    image: z.string().optional(),
 })
 
 const tokenMetadataSchema = z.object({
@@ -56,7 +56,11 @@ export const validateTonCenterResponse = async (
     const resultParams = response[expectedJettonParams.address.toRawString().toUpperCase()]
     expect(resultParams).toBeDefined()
     expect(resultParams.token_info[0].type).toBe("jetton_masters")
-    expect(resultParams.token_info[0].name).toBe(expectedJettonParams.metadata.name)
-    expect(resultParams.token_info[0].description).toBe(expectedJettonParams.metadata.description)
-    expect(resultParams.token_info[0].image).toBe(expectedJettonParams.metadata.image)
+    if (resultParams.is_indexed) {
+        expect(resultParams.token_info[0].name).toBe(expectedJettonParams.metadata.name)
+        expect(resultParams.token_info[0].description).toBe(
+            expectedJettonParams.metadata.description,
+        )
+        expect(resultParams.token_info[0].image).toBe(expectedJettonParams.metadata.image)
+    }
 }
